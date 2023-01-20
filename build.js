2c8032515f296a1ea10245731fb2db62efa9b82d
Scredi <m.petronio91@gmail.com>
2023-01-20 10:58:28 +0100

    [FIX] fix packages dependencies (#15)

--- build.js
+++ build.js
@@ -1,13 +1,11 @@
 const { build } = require('esbuild');
 const { sassPlugin } = require('esbuild-sass-plugin');
-const { dependencies } = require('./package.json');
 
 const appEntry = 'src/index.tsx';
 
 build({
   bundle: true,
   entryPoints: [appEntry],
-  external: Object.keys(dependencies),
   logLevel: 'info',
   minify: true,
   sourcemap: true,
